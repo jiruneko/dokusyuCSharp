a1861491465039e7920f8e28dea57cf91f51b035
jiruneko <oohikaru310@gmail.com>
2025-03-22 09:41:45 +0900

    fourth commit

---
+++ Iterator/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class IteratorBasic
+{
+    public IEnumerable<string> GetString()
+    {
+        yield return "あいうえお";
+        yield return "かきくけこ";
+        yield return "さしすせそ";
+    }
+
+    static void Main(string[] args)
+    {
+        var ite = new IteratorBasic();
+        foreach (var str in ite.GetString())
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
